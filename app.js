8c325f9cecd45c7b411a7e938d02ee69e102813c
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-10-04 17:42:10 +0530

    adding user model

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 connectDB();
+
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 4001;
 
